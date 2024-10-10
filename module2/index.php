ff987272bdc978f037ff013fd8a5f6cb08123a0a
amirspahiu77 <spahiuamir10@gmail.com>
2024-10-10 19:15:02 +0200

    Initial commit

---
+++ module2/index.php
@@ -0,0 +1,17 @@
+<?php
+  $school = "Digital School";
+ // echo "I love $school";
+
+
+  //int, float, string, boolean, array, object, resources, null
+
+  // simple arithmetic's +, -, *, /, %
+
+  $x = 120;
+  $y = 50;
+  echo $x + $y;
+  echo $x - $y;
+  echo $x * $y;
+  echo $x / $y;
+?>
+
