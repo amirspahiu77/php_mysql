c8b0c0bdbe47e53f3bc7f07666856fc48783435a
amirspahiu77 <spahiuamir10@gmail.com>
2024-10-31 18:45:02 +0100

    Add files via upload

---
+++ easychallenge.php
@@ -0,0 +1,11 @@
+<?php
+$employee = array{
+    array("John ", "29", "Software Engineer" ),
+    array("Ethan ", "33", "Designer" ),
+    array("Rick ", "41", "Manager" ),
+};
+
+echo $employee[0][0]. "Age: " . $employee[0][1] . ", Position: " .$emplotee[0][2]. ".<br>";
+echo $employee[1][0]. "Age: " . $employee[1][1] . ", Position: " .$emplotee[1][2]. ".<br>";
+echo $employee[2][0]. "Age: " . $employee[2][1] . ", Position: " .$emplotee[2][2]. ".<br>";
+?>
